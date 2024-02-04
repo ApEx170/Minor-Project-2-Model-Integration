77cd6187d87c89ce008004ce4e2f0891a7b71333
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2024-02-04 17:39:46 +0530

    Html page changed and more sample views created

--- LinguaRefine/spell_check/views.py
+++ LinguaRefine/spell_check/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'spell_check/index.html') # This line returns the index.html template
+
+def spell_check(request):
+    pass
+    
+def grammar_check(request):
+    pass
+ 
