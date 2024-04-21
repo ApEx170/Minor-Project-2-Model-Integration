bb601cfd51570638fc62d2831a0928f7c15af0fa
ApEx170 <prnautiyal340@gmail.com>
2024-04-21 15:01:45 +0530

    Integrated Model to the backend and updated index.txt

--- LinguaRefine/spell_check/views.py
+++ LinguaRefine/spell_check/views.py
@@ -7,31 +7,113 @@ import string
 from textblob import TextBlob
 import nltk
 import json
+import re
+import numpy as np
+from collections import Counter
+
 
 def index(request):
-    return render(request, 'spell_check/index.html')
+    return render(request, "spell_check/index.html")
 
 def grammar_check(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.body)
-        input_text = data.get('input_text', '')
-        
-        words = word_tokenize(input_text.translate(str.maketrans('', '', string.punctuation)))
-       
+        input_text = data.get("input_text", "")
+
+        words = word_tokenize(
+            input_text.translate(str.maketrans("", "", string.punctuation))
+        )
+
         words_not_found = [word for word in words if not wordnet.synsets(word)]
-        
-        corrections = {}
-        for word in words_not_found:
-            corrected_word = str(TextBlob(word).correct())
-            input_text = input_text.replace(word, corrected_word)
-            corrections[word] = corrected_word
-        
-        print(corrections)  
-        
-        happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+
+        # corrections = {}
+        # for word in words_not_found:
+        #     corrected_word = str(TextBlob(word).correct())
+        #     input_text = input_text.replace(word, corrected_word)
+        #     corrections[word] = corrected_word
+        # importing data
+        def read_corpus(filename):
+            with open(filename, "r", encoding="utf-8") as file:
+                lines = file.readlines()
+
+                words = []
+                for word in lines:
+                    words += re.findall(r"\w+", word.lower())
+            return words
+
+        def update_vocab(corpus):
+            return set(corpus)
+
+        # invoke this function
+        corpus = read_corpus(r"big_corpus.txt")
+        vocab = update_vocab(corpus)
+        words_count = Counter(corpus)
+        total_words_count = float(sum(words_count.values()))
+        word_probabs = {
+            word: words_count[word] / total_words_count for word in words_count.keys()
+        }
+
+        def split(word):  # why
+            return [(word[:i], word[i:]) for i in range(len(word) + 1)]
+
+        def delete(word):
+            return [left + right[1:] for left, right in split(word) if right]
+
+        def swap(word):
+            return [
+                left + right[1] + right[0] + right[2:]
+                for left, right in split(word)
+                if len(right) > 1
+            ]
+
+        def replace(word):  # abcdef...z
+            return [
+                left + center + right[1:]
+                for left, right in split(word)
+                if right
+                for center in string.ascii_lowercase
+            ]
+
+        def insert(word):  # abcdef...z
+            return [
+                left + center + right[1:]
+                for left, right in split(word)
+                for center in string.ascii_lowercase
+            ]
+
+        def level_one_edits(word):
+            return set((delete(word) + swap(word) + replace(word) + insert(word)))
+
+        def level_two_edits(word):
+            return set(e2 for e1 in level_one_edits(word) for e2 in level_one_edits(e1))
+
+        def check_correct_spelling(words_not_found, vocab, word_probabs):
+            correct_list = {}
+            for word in words_not_found:
+
+                if word in vocab:
+                    continue
+                # getting all suggesions
+                suggestions = level_one_edits(word) or level_two_edits(word) or [word]
+                best_guesses = [w for w in suggestions if w in vocab]
+                if not best_guesses:
+                    correct_list[word] = 0
+                suggestions_with_probabs = [w for w in best_guesses]
+                suggestions_with_probabs.sort(key=lambda x: x[1], reverse=True)
+                correct_list[word] = suggestions_with_probabs
+            return correct_list
+
+        happy_tt = HappyTextToText("T5", "vennify/t5-base-gramar-correction")
         args = TTSettings(num_beams=5, min_length=1)
         result = happy_tt.generate_text("grammar: " + input_text, args=args)
-        
-        return JsonResponse({'corrected_text': result.text, 'corrections': corrections})
 
-    return JsonResponse({'error': 'Only POST method is allowed'}, status=405)
+        return JsonResponse(
+            {
+                "corrected_text": result.text,
+                "corrections": check_correct_spelling(
+                    words_not_found, vocab, word_probabs
+                ),
+            }
+        )
+
+    return JsonResponse({"error": "Only POST method is allowed"}, status=405)
